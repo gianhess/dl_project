202ee4851b558f85db0e1ec848e0f6f498d90430
Gu-ddy <guglielmo.bonifazi.2000@gmail.com>
2024-01-15 22:49:16 +0100

    Final version

--- experiments/warm_up/run.py
+++ experiments/warm_up/run.py
@@ -1,2 +1,19 @@
-from utils.warm_up import warm_up_reg
-warm_up_reg(7,weight_factor=0.09)
+from utils.warm_up import warm_up_weight_decay
+import argparse
+
+def parse_args():
+    """Parse command line arguments."""
+    parser = argparse.ArgumentParser(description='Example computing NC (batch-wise)')
+    parser.add_argument('--seed', type=int, default=7, help='random seed')
+    parser.add_argument('--weight_decay', type=float, default=0.05, help='weight decay')
+
+    args = parser.parse_args()
+    return args
+
+def main():
+    args = parse_args()
+    warm_up_weight_decay(args.seed, weight_decay=args.weight_decay, max_epoch_warm_up=150)
+
+
+if __name__ == "__main__":
+    main()
--- experiments/warm_up/utils/viz.py
+++ experiments/warm_up/utils/viz.py
@@ -2,61 +2,6 @@ import matplotlib.pyplot as plt
 import pickle
 import pandas as pd
 
-def plot_pairwise_subplots(df, metrics1, metric2, interp=100, labely2=None):
-    if labely2 is None:
-        labely2 = metric2
-
-    x = df.index
-    y2 = df[metric2]
-
-    fig, axes = plt.subplots(nrows=2, ncols=2, figsize=(12, 8))
-    fig.suptitle(f'Subplots for {labely2} on CIFAR10', fontsize=16)
-
-    for i, ax in enumerate(axes.flatten()):
-        metric1 = metrics1[i] if i < len(metrics1) else metrics1[-1]  # Use the last metric if not enough provided
-        y1 = df[metric1]
-        y1_sp = df[f'{metric1}_SP']
-
-        line1, = ax.plot(x, y1, '-', color='red', label=metric1, linewidth=1)
-        ax.plot(x, y1_sp, '--', color='orange', label=f'{metric1}_SP', linewidth=1)
-        ax.set_xlabel('Epochs of Warm Up')
-        ax.set_ylabel(f'{metric1}', color='red')
-        ax.tick_params('y', colors='red')
-
-        ax2 = ax.twinx()
-        line2, = ax2.plot(x, y2, '-', color='blue', label=metric2, linewidth=1)
-        ax2.set_ylabel(labely2, color='blue')
-        ax2.tick_params('y', colors='blue')
-
-        # Additional lines for metric2_SP
-        y2_sp = df[f'{metric2}_SP']
-        ax2.plot(x, y2_sp, '--', color='purple', label=f'{metric2}_SP', linewidth=1)
-
-        ax.set_facecolor('#f0f0f0')  # Background color
-        ax.grid(True, linestyle='--', alpha=0.7)
-
-        if interp:
-            ax.axvline(x=interp, color='grey', linestyle='--', linewidth=1, label='Interpolation')
-
-        # Create individual legend for each subplot
-        lines = [line1, line2, ax.lines[-2], ax2.lines[-1]]
-        labels = [line.get_label() for line in lines]
-        ax.legend(lines, labels, loc='upper right')
-
-    fig.tight_layout(rect=[0, 0.03, 1, 0.95])  # Adjust the layout to avoid overlapping titles
-
-    plt.show()
-
-
-def subplots_from_seed(seed, metrics1, metric2, interp=100, labely2=None, trunc_rate=0.1):
-    results_path = f'results/seed{seed}/results.pkl'
-    with open(results_path, 'rb') as f:
-        results = pickle.load(f)
-        index = range(5, (len(results)+1)* 5, 5)
-        results = pd.DataFrame(results, index = index)
-    
-    results = results.iloc[int(len(results) * (trunc_rate)):]
-    plot_pairwise_subplots(results, metrics1, metric2, interp, labely2)
     
 
 
@@ -109,3 +54,154 @@ def plot_correlations_from_seed(seed, fixed_metric, changing_metrics):
         results = pd.DataFrame(results, index = index)
     
     plot_correlations(results, fixed_metric, changing_metrics)
+
+
+
+
+def get_results_df(seed, dir = 'results/'):
+    path = f'{dir}seed{seed}/results.pkl'
+    with open(path, 'rb') as f:
+        results = pickle.load(f)
+        index = range(5, (len(results)+1)* 5, 5)
+        results = pd.DataFrame(results, index = index)
+    return results
+
+def get_results(seed, dir = 'results/'):
+    path = f'{dir}seed{seed}/results.pkl'
+    with open(path, 'rb') as f:
+        results = pickle.load(f)
+    return results
+
+def plot_pairwise_subplots(df, metrics1, metric2, interp=100, labely2=None, start = None, end = None, title = None):
+
+    if end is None:
+        end = df.index[-1]
+
+    if start is None:
+        start = 5
+    start = int(start//5 -1)
+    end = int(end//5)
+
+    df = df.iloc[start:end]
+    if labely2 is None:
+        labely2 = metric2
+
+    x = df.index
+    y2 = df[metric2]
+
+    fig, axes = plt.subplots(nrows=2, ncols=2, figsize=(12, 8))
+    if title is not None: fig.suptitle(f'Subplots for {labely2} on CIFAR10', fontsize=16)
+
+    for i, ax in enumerate(axes.flatten()):
+        metric1 = metrics1[i] if i < len(metrics1) else metrics1[-1]  # Use the last metric if not enough provided
+        y1 = df[metric1]
+        y1_sp = df[f'{metric1}_SP']
+
+        line1, = ax.plot(x, y1, '-', color='red', label=metric1, linewidth=1)
+        #ax.plot(x, y1_sp, '--', color='orange', label=f'{metric1}_SP', linewidth=1)
+        ax.set_xlabel('Epochs of Warm Up', fontsize=13)
+        ax.set_ylabel(f'{metric1}', color='red', fontsize=13)
+        ax.tick_params('y', colors='red')
+
+        ax2 = ax.twinx()
+        line2, = ax2.plot(x, y2, '-', color='blue', label=metric2, linewidth=1)
+        ax2.set_ylabel(labely2, color='blue', fontsize=13)
+        ax2.tick_params('y', colors='blue')
+
+        # Additional lines for metric2_SP
+        y2_sp = df[f'{metric2}_SP']
+        #ax2.plot(x, y2_sp, '--', color='purple', label=f'{metric2}_SP', linewidth=1)
+
+        ax.set_facecolor('#f0f0f0')  # Background color
+        ax.grid(True, linestyle='--', alpha=0.7)
+
+        if interp:
+            ax.axvline(x=interp, color='grey', linestyle='--', linewidth=1, label='Interpolation')
+
+        # Create individual legend for each subplot
+        lines = [line1, line2]
+        labels = [line.get_label() for line in lines]
+        ax.legend(lines, labels, loc='upper right')
+
+    fig.tight_layout(rect=[0, 0.03, 1, 0.95])  # Adjust the layout to avoid overlapping titles
+
+    plt.show()
+
+
+def sliding_window_correlation(df, col1, col2, window_size):
+
+    window_size = window_size //5
+
+    # Initialize an empty list to store correlation values
+    correlations = []
+
+    # Iterate over the DataFrame with a sliding window
+    for i in range(len(df) - window_size + 1):
+        # Extract the window
+        window = df.iloc[i:i + window_size]
+
+        # Calculate the correlation between col1 and col2 in the window
+        correlation = window[col1].corr(window[col2])
+
+        # Append the correlation value to the list
+        correlations.append(correlation)
+
+    # Convert the list of correlations to a pandas Series
+    result_series = pd.Series(correlations, index=df.index[window_size - 1:])
+
+    return result_series
+
+def plot_correlation(dfs, col1, col2, window_size):
+        correlation_series = sliding_window_correlation(dfs[0], col1, col2, window_size)
+        for df in dfs[1:]:
+            # Calculate the correlation series
+            correlation_serie = sliding_window_correlation(df, col1, col2, window_size)
+            correlation_series = pd.concat([correlation_series,correlation_serie], axis = 1)
+        mean_correlation = correlation_series.mean(axis=1)
+        std_correlation = correlation_series.std(axis=1)
+ 
+    
+        # Plot the correlation series
+        plt.figure(figsize=(8, 6))
+        plt.title(f'Correlation between {col1} and {col2} with a window size of {window_size}')
+        plt.xlabel('Last Warm Up epoch in the window')
+        plt.ylabel(f'Correlation')
+        plt.plot(mean_correlation, color='red', linewidth=1, label='Mean')
+        plt.fill_between(mean_correlation.index, mean_correlation - std_correlation, mean_correlation + std_correlation,
+                         color='red', alpha=0.2, label='Std')
+        plt.legend(loc='lower right')
+        plt.grid()
+        ax = plt.gca()
+        ax.axhline(0, color='grey', linewidth=1, linestyle='--')
+        ax.set_facecolor('#f0f0f0')  # Background color
+        plt.show()
+
+
+
+def plot_metrics_with_error_bars(metric_names, y_label, metric_labels):
+    final_dfs = [pd.read_csv(f'results/seed{seed}/final_df.csv', index_col= 0) for seed in range(7,12)]
+
+    # plotting it
+    fig, ax = plt.subplots(figsize=(8, 5))
+    colors = plt.cm.get_cmap('plasma', len(metric_names))
+    for i, metric_name in enumerate(metric_names):
+        metric = final_dfs[0][metric_name]
+        for df in final_dfs:
+            metric = pd.concat([metric, df[metric_name]], axis = 1)
+        metric_mean = metric.mean(axis = 1)
+        metric_std = metric.std(axis = 1)
+        if i <2:
+            ax.plot(metric_mean, label=metric_labels[i], color= colors(i))
+            ax.fill_between(metric_mean.index, metric_mean - metric_std, metric_mean + metric_std, alpha=0.3, color=colors(i))
+        else:
+            # Gian's fault
+            ax.plot(metric_mean, label=metric_labels[i], color= 'green')
+            ax.fill_between(metric_mean.index, metric_mean - metric_std, metric_mean + metric_std, alpha=0.3, color='green')
+    ax.set_xlabel('Warm Up Epochs')
+    ax.set_ylabel(y_label)
+
+    ax.set_facecolor('#f0f0f0')
+    plt.title(f'Mean and Std of {y_label}')
+    ax.grid()
+    ax.legend(loc='upper left')
+    plt.show()
